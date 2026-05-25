6b2b25d902f15dfa68e2baa76b0bc4cf13144aee
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-24 22:05:25 -0500

    add mangohud

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,6 +3,6 @@
 set -ouex pipefail
 
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install -y steam gamescope-session tailscale asusctl rog-control-center
+dnf install -y steam mangohud gamescope-session tailscale asusctl rog-control-center
 
 systemctl enable podman.socket
