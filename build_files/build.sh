a54f9610e15a646223155881ff12f5835b146f99
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-24 21:53:48 -0500

    add gamescope

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,8 +2,7 @@
 
 set -ouex pipefail
 
-dnf copr enable -y lukenukem/asus-linux
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install -y steam tailscale asusctl rog-control-center
+dnf install -y steam gamescope-session tailscale asusctl rog-control-center
 
 systemctl enable podman.socket
