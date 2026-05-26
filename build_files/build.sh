c754dc12a5bc98e82a5de0cc85a968ee6e12e815
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-26 18:28:21 -0500

    auto enable touchpad workaround

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,3 +9,4 @@ cd /lib/firmware/cirrus
 sudo ln -sf cs35l41/bincfgs/cs35l41-dsp1-19_5dB.bincfg.xz cs35l41-dsp1-spk-prot-10431fb3-l0.bincfg.xz
 sudo ln -sf cs35l41/bincfgs/cs35l41-dsp1-19_5dB.bincfg.xz cs35l41-dsp1-spk-prot-10431fb3-r0.bincfg.xz
 
+systemctl enable reload-hid_asus.service
